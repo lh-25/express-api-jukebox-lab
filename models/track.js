a5c582df140e991c71981531680d79a0d13e65d3
lh-25 <sprinklecake25@gmail.com>
2024-11-09 19:19:24 -0600

    added track details to model

--- models/track.js
+++ models/track.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 
 const trackSchema = mongoose.Schema({
   title: {type: String, require: true},
-  artist: {type: String, require: true}
+  artist: {type: String, require: true},
+  album: {type: String, require: true},
+  duration: {type: String, require: true},
+  release_year: {type: Number, require: true},
+  song_image: {type: String, require: true},
+
 })
 
 const Track = mongoose.model('Track', trackSchema)
